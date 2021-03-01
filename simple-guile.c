d020f7aa3349c6b5629811d94a44ada92592a2d8
Michael Lingelbach <m.j.lbach@gmail.com>
2021-02-28 21:31:14 -0800

    First commit

---
+++ simple-guile.c
@@ -0,0 +1,16 @@
+#include <libguile.h>
+
+static void
+inner_main (void *closure, int argc, char ** argv)
+{
+	/* preparation */
+	scm_shell (argc, argv);
+	/* after exit */
+}
+
+int 
+main (int argc, char ** argv)
+{
+	scm_boot_guile (argc, argv, inner_main, 0);
+	return 0;
+}
